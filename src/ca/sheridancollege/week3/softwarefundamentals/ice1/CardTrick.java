617384080f6e611adb376fe3c833032d773b63da
FoxesNeedSleep <60157269+FoxesNeedSleep@users.noreply.github.com>
2020-01-23 16:42:18 -0500

    Update CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -14,6 +14,9 @@ public class CardTrick {
     public static void main(String[] args) {
         
         Card[] magicHand = new Card[7];
+        Card luckyCard = new Card;
+        luckyCard.setValue(7);
+        luckyCard.setSuit(Hearts);
 
         for (int i = 0; i < magicHand.length; i++) {
             Card card = new Card();
